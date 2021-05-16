4b08d9a1676a9e279f3adf2688520f067f33df94
Denilson <denilson020898@gmail.com>
2021-05-16 21:31:45 +0700

    failed dynamic lib, can't reproduce with updated code

---
+++ c07/01_dynamic_libraries/source/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void FindPrimes(int start, int end);
+
+int main() {
+  FindPrimes(3, 100000);
+  return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
