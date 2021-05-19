eb1aa1e686e70e14e02dd928f57fe635c7bf2396
Denilson <denilson@mplus.software>
2021-05-19 10:47:02 +0700

    chapter 8 complete

--- c08/source/index.js
+++ c08/source/index.js
@@ -5,9 +5,13 @@ const initialProductData = {
 
 const MAXIMUM_NAME_LENGTH = 50;
 const VALID_CATEGORY_IDS = [100, 101];
+const VALID_PRODUCT_IDS = [200, 301];
+
+let productModule = null;
+let orderModule = null;
 
 function validateName(name) {
-    const isValid = Module.ccall('ValidateName', 'number',
+    const isValid = productModule.ccall('ValidateName', 'number',
         ['string', 'number'],
         [name, MAXIMUM_NAME_LENGTH]);
 
@@ -16,50 +20,135 @@ function validateName(name) {
 
 function validateCategory(categoryId) {
     const arrayLength = VALID_CATEGORY_IDS.length;
-    const bytesPerElement = Module.HEAP32.BYTES_PER_ELEMENT;
-    const arrayPointer = Module._malloc((arrayLength * bytesPerElement));
-    Module.HEAP32.set(VALID_CATEGORY_IDS, (arrayPointer / bytesPerElement));
-    const isValid = Module.ccall('ValidateCategory',
+    const bytesPerElement = productModule.HEAP32.BYTES_PER_ELEMENT;
+    const arrayPointer = productModule._malloc((arrayLength * bytesPerElement));
+    productModule.HEAP32.set(VALID_CATEGORY_IDS, (arrayPointer / bytesPerElement));
+    const isValid = productModule.ccall('ValidateCategory',
         'number',
         ['string', 'number', 'number'],
         [categoryId, arrayPointer, arrayLength]
     );
 
-    Module._free(arrayPointer);
+    productModule._free(arrayPointer);
 
     return (isValid === 1);
 }
 
 function initializePage() {
-    document.getElementById("name").value = initialData.name;
+    document.getElementById("name").value = initialProductData.name;
     const category = document.getElementById("category");
     const count = category.length;
     for (let index = 0; index < count; index++) {
-        if (category[index].value === initialData.categoryId) {
+        if (category[index].value === initialProductData.categoryId) {
             category.selectedIndex = index;
             break;
         }
     }
+
+    let showEditProduct = true;
+    if ((window.location.hash) && (window.location.hash.toLowerCase() === "#placeholder")) {
+        showEditProduct = false;
+    }
+    switchForm(showEditProduct);
 }
 
-function getSelectedCategoryId() {
-    const category = document.getElementById("category");
-    const index = category.selectedIndex;
-    if (index !== -1) { return category[index].value; }
+function switchForm(showEditProduct) {
+    setErrorMessage("");
+    setActiveNavLink(showEditProduct);
+    setFormTitle(showEditProduct);
+
+    if (showEditProduct) {
+        if (productModule === null) {
+            productModule = new Module({
+                dynamicLibraries: ['validate_product.wasm']
+            });
+        }
+        showElement("productForm", true);
+        showElement("orderForm", false);
+    } else {
+        if (orderModule === null) {
+            console.log("MODULE");
+            orderModule = new Module({
+                dynamicLibraries: ['validate_order.wasm']
+            });
+        }
+        showElement("productForm", false);
+        showElement("orderForm", true);
+    }
+}
+
+function setActiveNavLink(editProduct) {
+    const navEditProduct = document.getElementById("navEditProduct");
+    const navPlaceOrder = document.getElementById("navPlaceOrder");
+    navEditProduct.classList.remove("active");
+    navPlaceOrder.classList.remove("active");
+
+    if (editProduct) {
+        navEditProduct.classList.add("active");
+    } else {
+        navPlaceOrder.classList.add("active");
+    }
+}
+
+function setFormTitle(editProduct) {
+    const title = (editProduct ? "Edit Product" : "Place Order");
+    document.getElementById("formTitle").innerText = title;
+}
+
+function showElement(elementId, show) {
+    const element = document.getElementById(elementId);
+    element.style.display = (show ? "": "none");
+}
+
+function getSelectedDropdownId(elementId) {
+    const dropdown = document.getElementById(elementId);
+    const index = dropdown.selectedIndex;
+    if (index !== -1) { return dropdown[index].value; }
     return "0";
 }
 
 function setErrorMessage(error) {
     const errorMessage = document.getElementById("errorMessage");
     errorMessage.innerText = error;
-    errorMessage.style.display = (error === "" ? "none" : "");
+    showElement("errorMessage", (error !== ""));
 }
 
-function onClickSave() {
+function onClickSaveProduct() {
     setErrorMessage("");
     const name = document.getElementById("name").value;
-    const categoryId = getSelectedCategoryId();
+    const categoryId = getSelectedDropdownId("category");
 
     if (validateName(name) && validateCategory(categoryId)) {
     }
+}
+
+function onClickAddToCart() {
+    setErrorMessage("");
+    const productId = getSelectedDropdownId("product");
+    const quantity = document.getElementById("quantity").value;
+
+    if (validateProduct(productId) && validateQuantity(quantity)) {
+    }
+}
+
+function validateProduct(productId) {
+    const arrayLength = VALID_PRODUCT_IDS.length;
+    const bytesPerElement = orderModule.HEAP32.BYTES_PER_ELEMENT;
+    const arrayPointer = orderModule._malloc((arrayLength * bytesPerElement));
+    orderModule.HEAP32.set(VALID_PRODUCT_IDS, (arrayPointer / bytesPerElement));
+    const isValid = orderModule.ccall('ValidateProduct',
+        'number',
+        ['string', 'number', 'number'],
+        [productId, arrayPointer, arrayLength]
+    );
+    orderModule._free(arrayPointer);
+    return (isValid === 1);
+}
+
+function validateQuantity(quantity) {
+    const isValid = orderModule.ccall("ValidateQuantity",
+        "number",
+        ["string"],
+        [quantity]);
+    return (isValid === 1);
 }
